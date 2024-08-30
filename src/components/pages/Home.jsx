e33758adce5fd4afe4e6c468e69fc2f4f2057eec
intigam <intiqamsumalov1@gmail.com>
2024-08-30 14:50:10 +0400

    cinfigration color tailwind

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='text-3xl text-cyan-500'>Home</div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      "my_white":"#FFFFFF",
+      "my_gray":"#F4F4F4",
+      "my_red":"#D3112A",
+      "my_orange":"#E52E05",
+      "my_dark_gray":"#E7EBEE"
+
+    }
+  },
+  plugins: [],
+}
